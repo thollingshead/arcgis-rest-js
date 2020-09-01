e01d88338f23f19665eeac4e7edb32f952642186
doug5997 <dcarroll@esri.com>
2020-09-01 09:27:54 -0700

    Update JSDoc w/ example and tweaked error message again

--- packages/arcgis-rest-portal/src/util/SearchQueryBuilder.ts
+++ packages/arcgis-rest-portal/src/util/SearchQueryBuilder.ts
@@ -3,9 +3,15 @@ import { IParamBuilder, warn } from "@esri/arcgis-rest-request";
  * `SearchQueryBuilder` can be used to construct the `q` param for [`searchItems`](/arcgis-rest-js/api/portal/searchItems#searchItems-search) or [`searchGroups`](/arcgis-rest-js/api/portal/searchGroups#searchGroups-search). By chaining methods, it helps build complex search queries.
  *
  * ```js
+ * const startDate = new Date("2020-01-01").valueOf();
+ * const endDate = new Date(2020-09-01).valueOf();
  * const query = new SearchQueryBuilder()
  *  .match("Patrick")
  *  .in("owner")
+ *  .and()
+ *  .from(startDate)
+ *  .to(endDate)
+ *  .in("created")
  *  .and()
  *  .startGroup()
  *    .match("Web Mapping Application")
@@ -27,7 +33,7 @@ import { IParamBuilder, warn } from "@esri/arcgis-rest-request";
  *
  * Will search for items matching
  * ```
- * "owner: Patrick AND (type:"Web Mapping Application" OR type:"Mobile Application" OR type:Application) AND Demo App"
+ * "owner: Patrick AND created:[1577836800000 TO 1598918400000] AND (type:"Web Mapping Application" OR type:"Mobile Application" OR type:Application) AND Demo App"
  * ```
  */
 export class SearchQueryBuilder implements IParamBuilder {
@@ -209,7 +215,7 @@ export class SearchQueryBuilder implements IParamBuilder {
     if (this.hasTerms) {
       warn(
         // prettier-ignore
-        `\`from(...)\` is not allowed after \`match(...)\` try using \`.from(...).to(...).in(...)\`. Optionally, you may see this because dates are incorrectly formatted, not in milliseconds.  Your query was not modified.`
+        `\`from(...)\` is not allowed after \`match(...)\` try using \`.from(...).to(...).in(...)\`. Optionally, you may see this because dates are incorrectly formatted. Dates should be a primative Date value, aka a number in milliseconds.  Your query was not modified.`
       );
       return this;
     }
@@ -231,7 +237,7 @@ export class SearchQueryBuilder implements IParamBuilder {
     if (this.hasTerms) {
       warn(
         // prettier-ignore
-        `\`to(...)\` is not allowed after \`match(...)\` try using \`.from(...).to(...).in(...)\`. Optionally, you may see this because dates are incorrectly formatted, not in milliseconds. Your query was not modified.`
+        `\`to(...)\` is not allowed after \`match(...)\` try using \`.from(...).to(...).in(...)\`. Optionally, you may see this because dates are incorrectly formatted. Dates should be a primative Date value, aka a number in milliseconds. Your query was not modified.`
       );
       return this;
     }
